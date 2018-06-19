e872fa0def5eb750dc750557de18fd695f8e0e89
Raif André <31712042+raifandre@users.noreply.github.com>
2018-06-19 19:50:51 -0300

    Add files via upload
    Banco de dados atualizado

--- Ecomerce - RAIF/banco de dados/database.sql
+++ Ecomerce - RAIF/banco de dados/database.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 12-Maio-2018 às 23:11
+-- Generation Time: 20-Jun-2018 às 00:49
 -- Versão do servidor: 10.1.16-MariaDB
 -- PHP Version: 5.6.24
 
@@ -23,13 +23,54 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `carrinho`
+--
+
+CREATE TABLE `carrinho` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `preco` int(11) NOT NULL,
+  `quantidade` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `carrinho`
+--
+
+INSERT INTO `carrinho` (`id`, `nome`, `preco`, `quantidade`) VALUES
+(14, 'Nike HyperVenom10', 1000, 3),
+(20, 'Mercurial Viper V', 500, 8);
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `categoria`
 --
 
 CREATE TABLE `categoria` (
   `id` int(11) NOT NULL,
-  `nome` varchar(50) NOT NULL,
-  `tipo` varchar(50) NOT NULL
+  `nome` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `categoria`
+--
+
+INSERT INTO `categoria` (`id`, `nome`) VALUES
+(2, 'Chuteira Campo'),
+(3, 'Chuteira Fustal'),
+(4, 'Chuteira Society 2');
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cupom`
+--
+
+CREATE TABLE `cupom` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(20) NOT NULL,
+  `desconto` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
@@ -41,19 +82,39 @@ CREATE TABLE `categoria` (
 CREATE TABLE `produto` (
   `id` int(11) NOT NULL,
   `nome` varchar(50) NOT NULL,
-  `categoria` varchar(20) NOT NULL,
+  `categoria` varchar(30) NOT NULL,
   `preco` varchar(20) NOT NULL,
   `descricao` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `produto`
+--
+
+INSERT INTO `produto` (`id`, `nome`, `categoria`, `preco`, `descricao`) VALUES
+(1, 'Mercurial Viper V', 'Chuteira', '500', 'Chuteira de campo'),
+(2, 'Nike HyperVenom10', 'Chuteira Campo', '1000', 'Chuteira de campo');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `carrinho`
+--
+ALTER TABLE `carrinho`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `categoria`
 --
 ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `cupom`
+--
+ALTER TABLE `cupom`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -67,15 +128,25 @@ ALTER TABLE `produto`
 --
 
 --
+-- AUTO_INCREMENT for table `carrinho`
+--
+ALTER TABLE `carrinho`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+--
 -- AUTO_INCREMENT for table `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `cupom`
+--
+ALTER TABLE `cupom`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `produto`
 --
 ALTER TABLE `produto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
